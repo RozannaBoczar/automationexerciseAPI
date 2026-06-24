4957a4586fe6cfc2c58e54db26cf113db793e16d
RozannaBoczar <rozanna.boczar@gmail.com>
2026-06-24 18:49:54 +0200

    Dodaj pliki projektów.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace automationexerciseAPI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
