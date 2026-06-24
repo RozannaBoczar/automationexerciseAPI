0ec31b4278e483b0a78bb7c8f6d1651384250ae7
RozannaBoczar <rozanna.boczar@gmail.com>
2026-06-24 22:07:46 +0200

    API tests

--- TestsRest/AutomationExerciseRestApiTests.cs
+++ TestsRest/AutomationExerciseRestApiTests.cs
@@ -20,7 +20,7 @@ namespace automationexerciseAPI.TestsRest
         }
 
         [Test]
-        public async Task PostAllProducts_ShouldReturnSuccessStatusCode()
+        public async Task PostAllProducts_ShouldReturnMethodNotAllowedStatusCode()
         {
             // Arrange
             var client = new RestSharpClientApi();
@@ -38,7 +38,42 @@ namespace automationexerciseAPI.TestsRest
             // Act
             var response = await client.GetAsync("brandsList");
             // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task PutAllBrandsList_ShouldReturnMethodNotAllowedStatusCode()
+        {
+            // Arrange
+            var client = new RestSharpClientApi();
+            // Act
+            var response = await client.PutAsync("brandsList");
+            // Assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.MethodNotAllowed);
+        }
+
+        [Test]
+        public async Task PostSearchProduct_ShouldReturnSuccessStatusCode()
+        {
+            // Arrange
+            var client = new RestSharpClientApi();
+            var body = new { search_product = "T-Shirt" };
+            // Act
+            var response = await client.PostAsync("searchProduct", body);
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task PostSearchProduct_ShouldReturnBadRequestStatusCode_WhenNoSearchProduct()
+        {
+            // Arrange
+            var client = new RestSharpClientApi();
+            // Act
+            var response = await client.PostAsync("searchProduct");
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+            response.ErrorMessage.Should().Be("search_product parameter is missing in POST request");
         }
     }
 }
