22de84445455400c8a3b99bd1bc4d12a89b70a01
RozannaBoczar <rozanna.boczar@gmail.com>
2026-06-24 20:23:24 +0200

    Add first rest sharp test

---
+++ HelpersRest/RestSharpClientApi.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+
+public class RestSharpClientApi
+{
+	private readonly RestClient _client;
+	public RestSharpClientApi(string baseUrl)
+	{
+		_client = new RestClient(baseUrl);
+	}
+	public async Task<RestResponse> GetAsync(string endpoint)
+	{
+		var request = new RestRequest(endpoint, Method.Get);
+		return await _client.GetAsync(request);
+	}
+	public async Task<RestResponse> PostAsync(string endpoint, object body)
+	{
+		var request = new RestRequest(endpoint, Method.Post);
+		request.AddJsonBody(body);
+		return await _client.PostAsync(request);
+	}
+	public async Task<RestResponse> PutAsync(string endpoint, object body)
+	{
+		var request = new RestRequest(endpoint, Method.Put);
+		request.AddJsonBody(body);
+		return await _client.PutAsync(request);
+	}
+	public async Task<RestResponse> DeleteAsync(string endpoint)
+	{
+		var request = new RestRequest(endpoint, Method.Delete);
+		return await _client.DeleteAsync(request);
+	}
+}
---
+++ TestsRest/AutomationExerciseRestApiTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace automationexerciseAPI.TestsRest
+{
+    [TestFixture]
+    public class AutomationExerciseRestApiTests
+    {
+        [Test]
+        public async Task GetAllProducts_ShouldReturnSuccessStatusCode()
+        {
+            // Arrange
+            var client = new RestSharpClientApi("https://automationexercise.com/api/");
+            // Act
+            var response = await client.GetAsync("productsList");
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
